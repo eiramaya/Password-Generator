3a52bd72b3f5664a348c9042d3c44298d917ecf2
eiramaya <107981414+eiramaya@users.noreply.github.com>
2022-06-23 11:46:13 +0800

    Password Generator

---
+++ PasswordGenerator2.py
@@ -0,0 +1,29 @@
+import string
+import random
+
+print("Hello, Welcome to Password Generator\n")
+
+## characters to generate password from
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_random_password():
+	## length of password from the user
+	length = int(input("Enter the lenght of password: "))
+
+	## shuffling the characters
+	random.shuffle(characters)
+	
+	## picking random characters from the list
+	password = []
+	for i in range(length):
+		password.append(random.choice(characters))
+
+	## shuffling the resultant password
+	random.shuffle(password)
+
+	## converting the list to string
+	## printing the list
+	print("\nHere is your password:","".join(password))
+
+## invoking the function
+generate_random_password()
